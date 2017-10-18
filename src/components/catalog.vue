55dddd679ec090e7d50ee6538c63a2060ec47c75
Sergey <sunliveua@gmail.com>
2017-10-18 12:39:35 +0300

    0.1.3

--- src/components/catalog.vue
+++ src/components/catalog.vue
@@ -1,11 +1,13 @@
 <template>
   <v-container fluid>
     <v-parallax src="/static/banner2.png" height="600">
-      <v-layout align-center justify-center>
+      <v-layout align-center justify-start>
         <v-flex xs12 sm6 lg4 class="text-xs-center">
-          <div class="capital"><span>Получи каталог стильной коллекции</span></div>
+          <div class="backgroundText">
+            <div>Получи каталог стильной коллекции</div>
+          </div>
           <div class="border"></div>
-          <div class="text"><span>Мы предоставим Вам перечень дизайнерской одежды</span></div>
+          <div class="text">Мы предоставим Вам перечень дизайнерской одежды</div>
           <v-form class="form text-xs-right">
             <v-text-field label="Ваше имя"></v-text-field>
             <v-text-field label="Ваш телефон"></v-text-field>
@@ -20,8 +22,16 @@
 
 <script>
   export default {
-    
+    // some code
   }
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+.text
+  font-size 1.3rem
+  color #a7a7a7
+  margin-bottom 2rem
+
+.border
+  margin-top 1.5rem
+</style>
