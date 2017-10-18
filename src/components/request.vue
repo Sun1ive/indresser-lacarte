899eb0c9067a24565a00a593549dcd63ee0d4ac5
Sergey <sunliveua@gmail.com>
2017-10-18 13:11:59 +0300

    0.1.6

--- src/components/request.vue
+++ src/components/request.vue
@@ -1,13 +1,17 @@
 <template>
   <v-container fluid>
-    <v-parallax src="/static/collectionBanner.png" height="700">
-      <v-layout justify-center>
-        <v-flex xs12 sm6 lg6 class="head">
-          <div class="backgroundText my-3"><span>Эту коллекцию очень быстро разбирают</span></div>
-          <div class="backgroundText my-3"><span>Эту коллекцию очень быстро разбирают</span></div>
-        </v-flex>
+    <v-parallax src="/static/collectionBanner.png" height="800">
+      <v-layout class="mt-5" justify-start align-center>
+        <div class="backgroundText">
+          <div>Эту коллекцию очень быстро разбирают</div>
+        </div>
       </v-layout>
-      <v-layout align-center justify-center>
+      <v-layout justify-end align-end>
+        <div class="backgroundText idk">
+          <div>Узнайте или остался Ваш размер</div>
+        </div>
+      </v-layout>
+      <v-layout align-center justify-end>
         <v-flex xs12 sm6 lg4>
           <v-form class="form text-xs-right">
             <v-text-field label="Ваше имя"></v-text-field>
@@ -28,12 +32,10 @@
 </script>
 
 <style scoped lang="stylus">
+.idk
+  min-width 630px
+
 .container
   margin-top -1rem
 
-.head
-  display flex
-  flex-direction column
-  align-items center
-  justify-content center
 </style>
